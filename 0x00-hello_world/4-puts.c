bb3e88d6cc662bab0c0d54d24b16d8b6c7c4a1a2
opeyemi olatunji <roseolatunji93@gmail.com>
2023-02-16 13:40:33 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *Description: 'a program to print out a line of program'
+*return: always 0(success)
+*/
+int main(void)
+{
+
+char puzzle[] = "Programming is like building a multilingual puzzle";
+puts(puzzle); /*writing the string to stdout*/
+return (0);
+}
