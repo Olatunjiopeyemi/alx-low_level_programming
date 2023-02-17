cb0af7dd13b1aa52b0aefadd2c6786b2ff92900c
opeyemi olatunji <roseolatunji93@gmail.com>
2023-02-17 14:08:19 -0800

    smile in the mirror

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry
+ * program that prints all possible combination of single digit
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchat(48 + n);
+		if (n != 9)
+		{
+		putchar(',');
+		putchar('');
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
+
