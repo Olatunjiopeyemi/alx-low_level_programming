e53185bc69124307fa284f3fe03ce1907ea688fe
opeyemi olatunji <roseolatunji93@gmail.com>
2023-02-17 07:09:45 -0800

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - entry point
+ * print all lowercase letters
+ * Return: 0
+ */
+
+	int main(void)
+{
+	for (ch = 'a'; ch <= 'z'; ch++)
+	putchar(ch);
+	putchar('\n');
+return (0);
+}
+
