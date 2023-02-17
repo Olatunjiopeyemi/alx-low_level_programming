592a43650abd47a34915004f6651a5638187c0cd
opeyemi olatunji <roseolatunji93@gmail.com>
2023-02-16 22:04:26 -0800

    Size is not grandeur, and territory does not make a nation

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints standard error
+ * Return: 1
+ */
+int main(void)
+{
+fprint(stderr, "and that piece of art is useful" Dora Korpar, 2015 10 19");
+return (1);
+}
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
+printf("Size of Char Data Types in C = %2d bytes \n", sizeof(char));
 printf("Size of Int Data Types in C = %2d bytes \n", sizeof(short int));
 printf("Size of Long Int Data Types in C = %2d bytes \n", sizeof(long int));
+printf("Size of long long int in C = %2d bytes \n", sizeof(long long int));
 printf("Size of Float Data Types in C = %2d bytes \n", sizeof(float));
-printf("Size of Double Data Types in C = %2d bytes \n", sizeof(double));
-printf("Size of Char Data Types in C = %2d bytes \n", sizeof(char));
 
 return (0);
 }
