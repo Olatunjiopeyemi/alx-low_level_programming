ffad74e9373f6b3bf08400ed2d89e18bffb0fbbc
opeyemi olatunji <roseolatunji93@gmail.com>
2023-02-21 08:35:20 -0800

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,12 +5,17 @@
 /**
  * _islower -> body of program
  *
- * c: characterr argument
+ * @c: characterr argument
  * a function that checks for lowercase character
- * Return: 0 or 1
+ * Return: 0 if true and 1 if false
  */
 
 int _islower(int c)
 {
-	return (c >= 'a' && c <= 'z');
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+
+
 }
