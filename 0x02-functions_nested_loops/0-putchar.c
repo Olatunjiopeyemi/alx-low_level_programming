bfdab98f0f4c703f135c9b056a3f1f5f178674ce
opeyemi olatunji <roseolatunji93@gmail.com>
2023-02-21 03:16:16 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,9 +6,16 @@
 */
 int main(void)
 {
-	char f[] = '_putchar';
 
-	putchar(f);
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
 
 	putchar('\n');
 	return (0);
