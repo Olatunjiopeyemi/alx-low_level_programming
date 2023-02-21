be915d7de160e5557dcfe664c5cf0d604807c3cc
opeyemi olatunji <roseolatunji93@gmail.com>
2023-02-21 10:27:15 -0800

    last digits

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+include "main.h"
+
+/**
+ * print_last_digit - body
+ *
+ *@x: argument
+ * a function that prints the sign of a number
+ *
+ * Return: value of the last digit
+ * last_digit: modulus 
+ */
+int print_last_digit(int x)
+{
+	last_digit = x % 10
+		if (last_digit > 0)
+			return (last_digit)
+}
