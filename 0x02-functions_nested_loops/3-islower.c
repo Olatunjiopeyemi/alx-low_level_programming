bf9f91e07400df6b23eab407fbff8f4e5882a969
opeyemi olatunji <roseolatunji93@gmail.com>
2023-02-21 06:05:29 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include "main.h"
+
+
+/**
+ * _islower - body
+ *
+ * int c - characterr argument
+ * a function that checks for lowercase character
+ * Return: 0 or 1 
+ */
+
+void print_alphabet_x10(void)
+{
+	return(c>= 'a' && c <= 'z');
+}
+
