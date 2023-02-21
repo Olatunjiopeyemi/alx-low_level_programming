76623e7c15791ed2c214d848aab939144cdcf4eb
opeyemi olatunji <roseolatunji93@gmail.com>
2023-02-21 10:06:11 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _abs - body
+ *
+ *@x: argument
+ * a function that prints the sign of a number
+ *
+ * Return: Absolute value
+ *
+ */
+
+int _abs(int x)
+{
+	if (x >= 0)
+	{
+		return (x);
+	}
+	else
+	{
+		return (-x);
+	}
+}
