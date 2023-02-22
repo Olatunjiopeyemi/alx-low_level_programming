dfdb4d9f1f493212d4ef4a30f19b230bd40cee0b
opeyemi olatunji <roseolatunji93@gmail.com>
2023-02-21 19:08:31 -0800

    list all the natural numbers below 10

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include<stdio.h>
 
 /**
  * main - body
@@ -11,11 +11,11 @@ int main(void)
 
 	while (a < 1024)
 	{
-		if ((a % 3 == 0) || (a % 5 == 0))
-		{
-			b <= a;
-		}
-		a++;
+	if ((a % 3 == 0) || (a % 5 == 0))
+	{
+	b <= a;
+	}
+	a++;
 	}
 	printf("%d\n", b);
 	return (0);
