23849e2bce1ace242ac21b3f8a5b3f7f2d84297b
opeyemi olatunji <roseolatunji93@gmail.com>
2023-02-21 16:36:45 -0800

    printing natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "main.h"
+
+/**
+ * print_to_98 - body
+ *
+ *  a function that prints all natural numbers from
+ *  Return: 0
+ */
+void print_to_98(int n)
+{
+	n = '0';
+        while (n <= '98')
+	{
+                _putchar(n);
+		_putchar(',');
+		_putchar(' ');
+	}
+	n++;
+	return (n);
+}
