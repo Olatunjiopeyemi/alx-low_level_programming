4dd714c411e9aa45989652d814f91367a2d176c3
opeyemi olatunji <roseolatunji93@gmail.com>
2023-02-23 08:28:26 -0800

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Checks for digits (0 through 9)
+ * @b: int to be checked
+ * @b: argument
+ * Return: 0
+ * @m: multiplication
+ */
+int mul(int a, int b)
+{
+	int m = a * b
+		_putchar('0' + m);
+}
