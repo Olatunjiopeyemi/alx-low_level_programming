40b982d2a6ef7ca8a3b33c285e632c3e38155f49
opeyemi olatunji <roseolatunji93@gmail.com>
2023-02-23 10:24:55 -0800

    prime factors

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Checks for checks for a digit (0 through 9)
+ * Return: 0 (Succesful)
+ */
+
+int main(void)
+{
+	long a, b;
+
+	a = 612852475143;
+
+	for (b = 2; a > b; b++)
+	{
+		while (a % b == 0)
+		{
+			a = a / a;
+		}
+	}
+	printf("%lu", y);
+	putchar('\n');
+	return (0);
+}
