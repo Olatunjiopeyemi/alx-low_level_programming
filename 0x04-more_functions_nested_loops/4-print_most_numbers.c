df6f7c9180b48bf851a08143f6622095bed006e1
opeyemi olatunji <roseolatunji93@gmail.com>
2023-02-23 09:12:12 -0800

    I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - body
+ */
+void print_most_numbers(void)
+{
+	char ope = '0';
+
+	while (ope <= '9' && ope != '2' && ope != '4')
+	{
+		_putchar(ope);
+	}
+}
