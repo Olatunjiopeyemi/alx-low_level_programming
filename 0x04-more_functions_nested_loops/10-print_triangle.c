112819fe5e1d333b1e01a575f665d37440dcdab7
opeyemi olatunji <roseolatunji93@gmail.com>
2023-02-23 10:17:02 -0800

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -17,12 +17,12 @@ void print_triangle(int size)
 
 		for (a = 1; a <= size; a++)
 		{
-			for (b = i; b < size; b++)
+			for (b = a; b < size; b++)
 			{
 				_putchar(' ');
 			}
 
-			for (b = 1; b <= i; b++)
+			for (b = 1; b <= a; b++)
 			{
 				_putchar('#');
 			}
