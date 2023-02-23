3c915f83b045ea31946087a454dbefd3cec69263
opeyemi olatunji <roseolatunji93@gmail.com>
2023-02-23 08:17:37 -0800

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,12 @@
 nclude "main.h"
 
 /**
- * _isdigit - Body
- * a function that checks for a digit (0 through 9).
+ * _isdigit - Body a function that checks for a digit (0 through 9).
  * @c: argument
  * Return: 0 or 1
  */
 int _isdigit(int c)
 {
-	return (c >= '0' && c < '10;)
+	return (c >= '0' && c < '10');
 }
 
