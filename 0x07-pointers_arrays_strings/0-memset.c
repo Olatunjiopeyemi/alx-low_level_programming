37f90b1107f2851a27fbfab76a3eab996f8d828b
opeyemi olatunji <roseolatunji93@gmail.com>
2023-03-06 13:08:38 -0800

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
-/*
+/**
  * _memset - body
- *  a function that fills memory with a constant byte
- *  @s: pointer variable
- *  @b: a constant byte
- *  @n: number of byte
- *  Return: a pointer to the memory area s
+ * a function that fills memory with a constant byte
+ * @s: pointer variable
+ * @b: a constant byte
+ * @n: number of byte
+ * Return: a pointer to the memory area s
  */
 
 char *_memset(char *s, char b, unsigned int n);
