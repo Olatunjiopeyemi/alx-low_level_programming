eb96d5f0753e95954e78d30e441853b985dc4a00
opeyemi olatunji <roseolatunji93@gmail.com>
2023-03-08 05:55:29 -0800

    Recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - body
+ * a function that prints a string, followed by a new line
+ * @s: string
+ */
+
+void void _print_rev_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+
+}
