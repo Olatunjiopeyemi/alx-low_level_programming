a58ac75407d17f327e239cfee57a7cda469597bc
opeyemi olatunji <roseolatunji93@gmail.com>
2023-03-10 14:52:38 -0800

    argc and argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,22 +7,66 @@
  * @argv: This is the array of arguments
  * Return: 0 if successful and 1 if not
  */
+
 int main(int argc, char *argv[])
-{       
-        int i, j;
-        int result;
+{
+	int result, num1, num2;
 
-	if(argc > 1)
+	if (argc < 3 || argc > 3)
 	{
-        	for (i = 1; i < argc; i++ && j = 1; j < argc; j++)
-        	{
-			result = atoi(argv[i]) * atoi(argv[j);
-		}
-		printf("%d\n", result);
+		printf("Error\n");
+		return (1);
 	}
-	return (0);
 
-	else 
-		printf("Error");
-	return (1);
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
 }
+
+/**
+ * _atoi - Converts a string to an integer
+ * @s: string to be converted
+ * Return: the int converted from the string
+ */
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int d = 0;
+	int n = 0;
+	int f = 0;
+	int len = 0;
+	int digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
+
