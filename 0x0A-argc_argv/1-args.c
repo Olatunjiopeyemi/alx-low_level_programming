fccc9a550a1a3487160f941a7ae1b0753482a3d9
opeyemi olatunji <roseolatunji93@gmail.com>
2023-03-10 13:15:07 -0800

    agrc and argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - body
+ * a program that prints the number of arguments passed into it
+ * @argc: the number of argument passed
+ * @argv: the array taht stores the address of the arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d/n", argc);
+	return (0);
+}
