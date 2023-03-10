8b3ace415af26526c70ce99486c9d360cd7c2975
opeyemi olatunji <roseolatunji93@gmail.com>
2023-03-10 14:32:11 -0800

    argv and argc

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -14,7 +14,10 @@ int main(int argc, char *argv[])
 
 	if (argc == 1)
 		printf("%d\n", argc - 1);
-	else if (x++; argv++);
-		printf("%d\n", x - 1)
+	else
+	{
+		for(x = 0; *argv; x++, argv++);
+		printf("%d\n", x - 1);
+	}
 	return (0);
 }
