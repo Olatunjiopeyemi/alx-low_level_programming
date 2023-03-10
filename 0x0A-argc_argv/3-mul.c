7218e6a39c080109f0e1f7f844622b8843149498
opeyemi olatunji <roseolatunji93@gmail.com>
2023-03-10 15:03:52 -0800

    argc and argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,32 +1,6 @@
 #include <stdio.h>
 #include "main.h"
 
-/**
- * main - body
- * a program that multiplies two numbers
- * @argc: number of arguments on a command line
- * @argv: array of arguments addresses
- * Return: 0 (Successful), 1 (Error)
- */
-
-int main(int argc, char *argv[])
-{
-	int result, num1, num2;
-
-	if (argc < 3 || argc > 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
-	result = num1 * num2;
-
-	printf("%d\n", result);
-
-	return (0);
-}
 
 /**
  * _atoi - Converts a string to an integer
@@ -76,4 +50,31 @@ int _atoi(char *s)
 		return (0);
 
 	return (n);
+}
+
+/**
+ * main - body
+ * a program that multiplies two numbers
+ * @argc: number of arguments on a command line
+ * @argv: array of arguments addresses
+ * Return: 0 (Successful), 1 (Error)
+ */
+
+int main(int argc, char *argv[])
+{
+        int result, num1, num2;
+
+        if (argc < 3 || argc > 3)
+        {
+                printf("Error\n");
+                return (1);
+        }
+
+        num1 = _atoi(argv[1]);
+        num2 = _atoi(argv[2]);
+        result = num1 * num2;
+
+        printf("%d\n", result);
+
+        return (0);
 }
