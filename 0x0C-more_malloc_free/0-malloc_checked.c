7172e0a5cc8a1acf54d3a378cf5c402220467351
opeyemi olatunji <roseolatunji93@gmail.com>
2023-03-16 12:48:36 -0700

    more om malloc and free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - A function that allocates memory using malloc.
+ * @b: the integer whose address is stored.
+ * Return: 98
+ */
+
+void *malloc_checked(unsigned int b)
+{       
+        void *new;
+
+	new = malloc(b);
+
+	if (new == '\0')
+		exit(98);
+
+
+	return(new);
+}
