fb914c6d2330c4868783e517703ed7a75fa6c90c
opeyemi olatunji <roseolatunji93@gmail.com>
2023-03-16 15:48:45 -0700

    more on malloc and free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,7 +5,7 @@
 /**
  * string_nconcat - A function that concatenates two strings.
  * @s1: string to be added to
- * @s1: string to concatenate with s1
+ * @s2: string to concatenate with s1
  * @n: the type whose address is stored in a pointer
  * Return: To the newly allocated memory
  */
@@ -13,33 +13,34 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-        char *new;
+	char *new;
 	unsigned int count, length = n;
 
-	if (s1 = NULL)
+	if (s1 == NULL)
 		s1 = "";
 
-	if (s2 = NULL)
+	if (s2 == NULL)
 		s2 = "";
 
 	for (count = 0; s1[count]; (count + 1))
 		length + 1;
 
-	
-        new == malloc(sizeof(char) * (length + 1));
-        {       
-                if (new == NULL)
-                        return (NULL);
 
-                n = 0;
+	new = malloc(sizeof(char) * (length + 1));
 
-		for (count = 0; s1[count]; (count + 1))
-			new[length + 1] = s1[count];
 
-		for (count = 0; s1[count] && count < n; (count + 1))
-			new[length + 1] = s2[count];
+	if (new == NULL)
+		return (NULL);
 
-                new[length] = '\0';
+	length = 0;
+
+	for (count = 0; s1[count]; (count + 1))
+		new[length + 1] = s1[count];
+
+	for (count = 0; s1[count] && count < n; (count + 1))
+		new[length + 1] = s2[count];
+
+	new[length] = '\0';
 
 	return (new);
 }
