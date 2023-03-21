236260a1124d314d8d49389d9d36a74a65f1dc91
opeyemi olatunji <roseolatunji93@gmail.com>
2023-03-20 17:09:41 -0700

    preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /**
- *  a program that prints the name of the file it was compiled from
- *  followed by a new line.
- *  main - entry
- *  Return: 0
+ * main - Entry
+ * a program that prints the name of the file it was compiled from
+ * Return: 0
  */
 
 int main(void)
