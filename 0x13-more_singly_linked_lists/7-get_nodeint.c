aa9596945089730c1d5c08a00015213784db8fab
Olatunji opeyemi <roseolatunji93@gmail.com>
2023-04-04 10:52:45 -0700

    more on singly linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,12 +12,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp = head;
 	unsigned int i;
 
-	i = 0;
 
 	if (!head || !*head)
 		return (NULL);
 
-	for (i = 0; temp->next && i < index; i++)
+	for (i = 0; temp && i < index; i++)
 	{
 		temp = temp->next;
 	}
