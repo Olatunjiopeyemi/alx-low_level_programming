f7f78f0e6f7b323c612d144efdeaa939e8f162a3
Olatunji opeyemi <roseolatunji93@gmail.com>
2023-04-04 10:39:20 -0700

    more on singly linked list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * free_listint - A function that frees a list
+ * free_listint2 - A function that frees a list
  * @head: The pointer to the beginning node
  */
 
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,6 +3,8 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stddef.h>
+#include <string.h>
 
 /**
  * struct listint_s - singly linked list
