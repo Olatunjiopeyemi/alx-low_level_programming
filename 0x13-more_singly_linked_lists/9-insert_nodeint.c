61f6e3f9a9e7b658357624e6f11dbe1165454373
Olatunji opeyemi <roseolatunji93@gmail.com>
2023-04-05 14:00:30 -0700

    more on singly linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *New, *temp;
 	unsigned int i;
 
-	temp = head
+	temp = head;
 
 	New = malloc(sizeof(listint_t));
 	if (!head || !New)
@@ -41,5 +41,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	temp->next = New;
 
 
-	return (new);
+	return (New);
 }
