50a59923ace74d2d1159c0b728dc8d6357f7e7e5
Olatunji opeyemi <roseolatunji93@gmail.com>
2023-04-05 13:55:20 -0700

    more on singly linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -6,6 +6,7 @@
  * @head: A pointer to the head node
  * @idx: The index of the list where the new node will be
  * @n: content of the new node
+ * Return: The address to the new node or NULL if no list
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
@@ -23,20 +24,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		New->next = *temp;
+		New->next = temp;
 		*head = New;
 		return (New);
 	}
 
-	i = 0;
-
-	while (i < (idx - 1))
+	for (i = 0; i < (idx - 1); i++)
 	{
 		if (temp == NULL || temp->next == NULL)
 			return (NULL);
 
 		temp = temp->next;
-		i++;
 	}
 
 	New->next = temp->next;
