8297353297386f87525ef69ea7f1b29db9fc1d64
Olatunji opeyemi <roseolatunji93@gmail.com>
2023-04-08 22:42:51 -0700

    files input and output handling

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,4 +9,36 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	
+	char *buffer;
+	int open, write, length = 0;
+
+	buffer = malloc(sizeof(char));
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULLL)
+	{
+		open == open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+		if (open == -1)
+			return (-1);
+
+		close(open);
+
+		return (1);
+	}
+
+	for (text_content[length]; length++)
+
+	open = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	write = write(o, text_content, length);
+
+	if (open == -1)
+		return (-1);
+	if (write == -1)
+		return (-1);
+
+	close(open);
+
+	return (1);
+}
