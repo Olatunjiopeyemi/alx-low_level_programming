bea3e0bc9bf76c45613abf33c7c3f66628974ed3
Olatunji opeyemi <roseolatunji93@gmail.com>
2023-04-10 06:17:49 -0700

    files input and output handling

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * read_textfile - A  function that reads a text file and prints it to the POSIX standard output.
+ * read_textfile - A  function that reads a text file and
+ * prints it to the POSIX standard output.
  * @filename: The file name
  * @letters: Its is the number of letters it should read and print
  * Return: The actual number of letters it could read and print
@@ -16,6 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
+
 	if (buffer ==  NULL)
 		return (0);
 
@@ -26,13 +28,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
-		return(0);
+		return (0);
 	}
 
 	free(buffer);
 	close(o);
 
-	return(w);
+	return (w);
 }
 
 
