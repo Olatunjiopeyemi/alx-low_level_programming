b6fb683da3db7e0517ddcc89613a68806f379668
Olatunji opeyemi <roseolatunji93@gmail.com>
2023-04-10 16:40:34 -0700

    bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,11 +9,11 @@
 
 void print_binary(unsigned long int n)
 {
+	int ope = 100;
+	for (ope = 63; ope >= 0; ope--)
+	{
+		_putchar((n >> ope) & 1 ? '1' : '0');
+	}
 
-	if (n >= 1)
-		print_binary(n >> 1);
 
-	_putchar((n & 1) ? '1' : '0');
-
-	_putchar('\n');
 }
