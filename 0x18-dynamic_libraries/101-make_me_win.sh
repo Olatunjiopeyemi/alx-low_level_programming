3728068681c04ab70602b7579a82a2db07a60b4c
opeyemi olatunji <roseolatunji93@gmail.com>
2023-06-19 14:52:10 -0700

    dynamic library

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ../ https://github.com/Olatunjiopeyemi/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so
-export LD_PRELOAD=../iwin.so
+wget -P ../ https://github.com/Olatunjiopeyemi/alx-low_level_programming/raw/main/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=../nrandom.so
