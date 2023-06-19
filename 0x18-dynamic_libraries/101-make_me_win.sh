5f3e05174c5caf831861381b671e7e5007e4a0ff
opeyemi olatunji <roseolatunji93@gmail.com>
2023-06-19 14:33:24 -0700

    dynamic library

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P ../ https://github.com/Olatunjiopeyemi/alx-low_level_programming/raw/main/0x18-dynamic_libraries/jackpot.so
-export LD_PRELOAD=/tmp/jackpot.so
+export LD_PRELOAD=../jackpot.so
